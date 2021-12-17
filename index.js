b36737eaba1328bea5a506ffeba00ea156da8321
FIarlori <f.iarlori@hotmail.com>
2021-12-17 17:23:12 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const packjson = require("./package.json");
+
+app.get("/health", function (req, res) {
+  res.json({
+    name: packjson.name,
+    version: packjson.version,
+  });
+});
+
+app.listen(port, function (req, res) {
+  console.log("API rest running at port" + port);
+});
